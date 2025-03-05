6f177331e260dec44b3dd67f741cf28cbdb9207d
Ger O'Connell <ger@yourapp.ie>
2025-03-05 22:00:47 +0000

    Menu Prompt moved down below navigation header

--- components/menu/MenuPrompt.js
+++ components/menu/MenuPrompt.js
@@ -1,18 +1,15 @@
-// import { useViewportSize } from "./ViewportSizeHook";
-
-
 const MenuPrompt = ({ setMenuVisible }) => {
   return (
     <div className='menuPrompt'
       onClick={() => setMenuVisible(true)}
       style={{
         position: 'absolute',
-        top: '5vh',
+        top: '7vh',
         right: '5vw',
+        // float: 'right',
         opacity: '75%',
         cursor: 'pointer',
       }}>
-      {/* {useViewportSize().screenWidth > 600 ? */}
       <div className='menuWord'
           style={{
             fontSize: '5vw',
@@ -23,11 +20,6 @@ const MenuPrompt = ({ setMenuVisible }) => {
           }}>
           MENU
         </div>
-        {/* : <div className='dkkBurger'>
-          <img src='/images/icons/burger.png' alt='burger icon'
-            style={{ width: '40px', height: '40px' }} />
-        </div>
-      } */}
     </div>
   );
 }
